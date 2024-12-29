942f31d70969c7b159d5d5171df14cad277c4b77
lpachecob <luis.pacheco.brophy.contractor@bbva.com>
2024-12-29 17:57:21 -0500

    feat(manifest): update version to 1.3.4
    feat(smelt): add condition to check if smelting is enabled and improve status logging

--- src/modules/smelt.js
+++ src/modules/smelt.js
@@ -21,42 +21,44 @@ const smelt = {
     );
   },
   async start() {
-    statusLog.innerText = "Iniciando Fundición";
-    smelting = true;
-    statusLog.innerText = "Buscando forjas";
-    let forges = await info.getForges();
-    forges.finished.forEach(async (forge) => {
-      await info.storeResourcesFromSmelt(forge["forge_slots.slot"]);
-    });
+    if (store.data.smelt.enable) {
+      statusLog.innerText = "Iniciando Fundición";
+      smelting = true;
+      statusLog.innerText = "Buscando forjas";
+      let forges = await info.getForges();
+      forges.finished.forEach(async (forge) => {
+        await info.storeResourcesFromSmelt(forge["forge_slots.slot"]);
+      });
 
-    if (forges.closed.length === 0) {
-      statusLog.innerText = "No hay forjas disponibles";
-      smelting = false;
-      return;
-    }
-    statusLog.innerText = "forja encontrada";
-    let forgeToSmelt = forges.closed[0]["forge_slots.slot"];
-    statusLog.innerText = "Buscando items para fundir";
-    let itemToSmeltId = await info.getItemForSmelt();
-
-    if (!itemToSmeltId) {
-      statusLog.innerText = "No hay items para fundir";
-      const formId = await info.getItemForSmeltInPackages();
-
-      if (formId) {
-        statusLog.innerText = "Buscando items para fundir en paquetes";
-        await info.collectPackage(formId, { x: 7, y: 3 });
-        statusLog.innerText = "Item encontrado";
-        itemToSmeltId = await info.getItemForSmelt();
-      }
-      if (!smeltingBagItems) {
-        statusLog.innerText = "No hay items para fundir";
+      if (forges.closed.length === 0) {
+        statusLog.innerText = "No hay forjas disponibles";
         smelting = false;
         return;
       }
-    }
+      statusLog.innerText = "forja encontrada";
+      let forgeToSmelt = forges.closed[0]["forge_slots.slot"];
+      statusLog.innerText = "Buscando items para fundir";
+      let itemToSmeltId = await info.getItemForSmelt();
 
-    statusLog.innerText = "Fundiendo items";
-    await info.smeltResourcesFromSmelt(forgeToSmelt, itemToSmeltId);
+      if (!itemToSmeltId) {
+        statusLog.innerText = "No hay items para fundir";
+        const formId = await info.getItemForSmeltInPackages();
+
+        if (formId) {
+          statusLog.innerText = "Buscando items para fundir en paquetes";
+          await info.collectPackage(formId, { x: 7, y: 3 });
+          statusLog.innerText = "Item encontrado";
+          itemToSmeltId = await info.getItemForSmelt();
+        }
+        if (!smeltingBagItems) {
+          statusLog.innerText = "No hay items para fundir";
+          smelting = false;
+          return;
+        }
+      }
+
+      statusLog.innerText = "Fundiendo items";
+      await info.smeltResourcesFromSmelt(forgeToSmelt, itemToSmeltId);
+    }
   },
 };
